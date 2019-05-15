721a59d7fe08f62bf856b6c8d62c45360dca4f90
georgec <georgeclark@ctuir.org>
2019-05-15 09:08:59 -0700

    Reset migrations.

--- services/Migrations/201905151606421_reset.Designer.cs
+++ services/Migrations/201905151606421_reset.Designer.cs
@@ -13,7 +13,7 @@ namespace services.Migrations
         
         string IMigrationMetadata.Id
         {
-            get { return "201905071748401_reset"; }
+            get { return "201905151606421_reset"; }
         }
         
         string IMigrationMetadata.Source
