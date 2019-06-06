6eaf4b3496ef7848ae624ab1d5f79a758638ef55
georgec <georgeclark@ctuir.org>
2019-06-06 16:55:20 -0700

    Added header, detail, and main class for CreelPhone.

---
+++ services/Models/Data/CreelPhone_Header.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace services.Models.Data
+{
+    public class CreelPhone_Header : DataHeader
+    {
+        //public DateTime? ContactDate { get; set; }
+        public int FishermanId { get; set; }
+        public string Fished { get; set; }
+        public string InterviewComments { get; set; }
+    }
+}
