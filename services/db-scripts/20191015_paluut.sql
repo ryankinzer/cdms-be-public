7fb6820168c4f98d4cb14b23b0835bc432b78b80
Ken Burcham <kenburcham@gmail.com>
2019-10-21 15:46:07 -0700

    add IN to permittypes

--- services/db-scripts/20191015_paluut.sql
+++ services/db-scripts/20191015_paluut.sql
@@ -1,1 +1,5 @@
-﻿
+﻿-- add IN to permittypes
+
+insert into permittypes (name, permittypestatus, permitnumberprefix, feerequired, stamprequired, currentpermitnumber)
+values ('Information',0,'IN',0,0,1)
+
