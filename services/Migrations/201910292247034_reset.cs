74d8ba78a85f50131bffc1ac2268307d201298be
Ken Burcham <kenburcham@gmail.com>
2019-11-12 14:32:03 -0800

    completed merge from test

--- services/Migrations/201910292247034_reset.cs
+++ services/Migrations/201910292247034_reset.cs
@@ -7,11 +7,7 @@ namespace services.Migrations
     {
         public override void Up()
         {
-<<<<<<< HEAD:services/Migrations/201911041620253_reset.cs
-           
-=======
-  
->>>>>>> test:services/Migrations/201910292247034_reset.cs
+
         }
         
         public override void Down()
