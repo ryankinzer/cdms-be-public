7c921417cd791a271d258fa4c7f2ab35b28ab030
georgec <georgeclark@ctuir.org>
2020-04-09 09:14:51 -0700

    Revised debug lines.

--- services/Resources/DatabaseColumnHelper.cs
+++ services/Resources/DatabaseColumnHelper.cs
@@ -14,6 +14,8 @@ namespace services.Resources
         private static Logger logger = LogManager.GetCurrentClassLogger();
 
         public static void addFieldToDatabase(Field in_field){
+            logger.Debug("Inside DatabaseColumnHelper.cs, addFieldToDatabase...");
+            //logger.Debug("in_field.DbColumnName = " + in_field.DbColumnName);
             
             var db = ServicesContext.Current;
 
@@ -28,7 +30,7 @@ namespace services.Resources
             var query = "ALTER TABLE " + tableName + " ADD " + in_field.DbColumnName + " ";
             query += getSQLFieldType(in_field.DataType, in_field.ControlType) + " NULL";
 
-            logger.Debug(query);
+            logger.Debug("query = " + query);
 
             using (SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["ServicesContext"].ConnectionString))
             {
