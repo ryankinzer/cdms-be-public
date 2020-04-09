7fbe7a1c29dfa5c51596e46dbe0d8aea1608c7d8
georgec <georgeclark@ctuir.org>
2020-04-09 07:57:58 -0700

    Added class for new Characteristics table.

---
+++ services/Models/Characteristics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+
+namespace services.Models
+{
+    public class Characteristics
+    {
+        public const int ACTIVE_STATUSID = 0;  // Active
+        public const int INACTIVE_STATUSID = 1;  // Inactive
+
+        public int Id { get; set; }
+        public string CharacteristicName { get; set; }
+        public int CharacteristicActive { get; set; }
+
+        public Characteristics()
+        {
+            CharacteristicActive = ACTIVE_STATUSID;
+        }
+    }
+}
